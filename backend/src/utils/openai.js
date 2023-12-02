afae74dbac718405d0a3417be3bfeb86874d0a8c
leo5470 <61446148+leo5470@users.noreply.github.com>
2023-12-02 21:25:06 +0800

    configure openai settings to use local model

--- backend/src/utils/openai.js
+++ backend/src/utils/openai.js
@@ -2,7 +2,10 @@ const OpenAI = require('openai')
 
 const transformMessages = require('./helper')
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+const openai = new OpenAI({
+    //  apiKey: process.env.OPENAI_API_KEY, 
+     apiKey: '', // The local model does not need api key.
+     baseUrl: process.env.OPENAI_API_BASE_URL})
 
 // 傳入 messages，取得 OpenAI API 的回覆
 const openAIChat = async (messages) => {
@@ -15,7 +18,8 @@ const openAIChat = async (messages) => {
         // 呼叫 openai api
         const completion = await openai.chat.completions.create({
             messages,
-            model: 'gpt-3.5-turbo',
+            // model: 'gpt-3.5-turbo',
+            model: process.env.OPENAI_API_MODEL,
             //max_tokens: 80,
             temperature: 0.9,
         })
