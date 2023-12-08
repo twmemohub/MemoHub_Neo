58855c31eb9507afbc55955e3aa657e8446995a0
leo5470 <61446148+leo5470@users.noreply.github.com>
2023-12-08 18:35:19 +0800

    Modify OpenAI calling

--- backend/src/utils/openai.js
+++ backend/src/utils/openai.js
@@ -5,7 +5,7 @@ const transformMessages = require('./helper')
 const openai = new OpenAI({
     //  apiKey: process.env.OPENAI_API_KEY, 
      apiKey: '', // The local model does not need api key.
-     baseUrl: process.env.OPENAI_API_BASE_URL})
+     baseURL: process.env.OPENAI_API_BASE_URL})
 
 // 傳入 messages，取得 OpenAI API 的回覆
 const openAIChat = async (messages) => {
@@ -16,13 +16,14 @@ const openAIChat = async (messages) => {
         // 轉換成 openai 格式
         messages = transformMessages(messages)
         // 呼叫 openai api
-        const completion = await openai.chat.completions.create({
+        const call = {
             messages,
             // model: 'gpt-3.5-turbo',
             model: process.env.OPENAI_API_MODEL,
             //max_tokens: 80,
-            temperature: 0.9,
-        })
+        }
+        console.log(call)
+        const completion = await openai.chat.completions.create(call)
         return completion.choices[0]
     }
 }
