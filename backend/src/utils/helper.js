ec167c2bc0374f00916495c3f36ad1fc543c42ac
leo5470 <61446148+leo5470@users.noreply.github.com>
2023-12-08 18:34:51 +0800

    Modify Helper Settings

--- backend/src/utils/helper.js
+++ backend/src/utils/helper.js
@@ -1,6 +1,7 @@
 const transformMessages = (list) => {
     return list.map((message, index) => {
-        const role = index % 2 === 0 ? 'user' : 'system'
+        // 0: system, 1: user, 2: assistant, 3: user...
+        const role = index == 0 ? 'system' : (index % 2 !== 0 ? 'user' : 'assistant')
         return {
             role: role,
             content: message,
